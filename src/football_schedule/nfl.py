a2f31c696c6fbe5008c57714435a75dcafd007a3
Markus Hermann <hermann.markus@gmail.com>
2023-01-09 14:14:27 +0100

    fix: Use correct case for UTF-8 on debian

--- src/football_schedule/nfl.py
+++ src/football_schedule/nfl.py
@@ -9,7 +9,7 @@ import rich_click as click
 from .output import output_table
 
 try:
-    locale.setlocale(locale.LC_TIME, "de_DE.uft-8")
+    locale.setlocale(locale.LC_TIME, "de_DE.UFT-8")
 except locale.Error:
     try:
         locale.setlocale(locale.LC_TIME, "de_DE")
